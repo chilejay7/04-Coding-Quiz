3c6a1787d987e3cbde5caad0c70a168e77c6420b
Cody Burkholder <codyburk7@yahoo.com>
2023-09-08 13:59:35 -0600

    added html, css, and js  files

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+// Create buttons in html document and add listeners.
+
+// Start button that triggers a countdown event with interval.  Game ends when clock reaches 0 or there aren't any addtional questions.  Click listener needed for Start button that triggers interval function.
+
+// Generate random questions.  Four potential answers with buttons to select answer.  This should probably be an ol to generate the four button elements with possible answers.
+
+// Answering question triggers next question.
+
+// If question answered incorrectly time subtracted from clock.
+
+// Form element to output score.
